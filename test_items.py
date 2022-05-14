4eaef3638128a9c613801bf10bdbf39e05d0b61f
Torchun69 <torchun69@gmail.com>
2022-05-14 13:56:27 +0400

    Тест. Наличие кнопки добавления в корзину

--- test_items.py
+++ test_items.py
@@ -1,11 +1,11 @@
-import pytest
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-@pytest.fixture(scope="function")
-def test_guest_should_see_login_link(browser):
+def test_find_bascet_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    time.sleep(5)
+    browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    y = len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-two-basket"))
+    assert y>0, '==КНОПКА НЕ НАЙДЕНА=='
