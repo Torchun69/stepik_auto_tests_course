d638679157bef6fa1857a4cf39c882da2c5f088d
Torchun69 <torchun69@gmail.com>
2022-05-14 15:37:42 +0400

    Тест. Поиск кнопки добавить в корзину

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_find_bascet_button(browser):
+def test_find_basket_button(browser):
     browser.get(link)
     time.sleep(5)
     browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
